b33667c114c3af015dc5262c3533bd5c88d402ec
atharva <atharvakadlag@gmail.com>
2020-06-23 22:13:23 +0530

    ready to deploy

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
