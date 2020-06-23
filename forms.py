fd62a5c3c39f09a83725f79e18aec7999787babc
atharva <atharvakadlag@gmail.com>
2020-06-23 20:55:46 +0530

    contact form

--- forms.py
+++ forms.py
@@ -12,8 +12,8 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config["MAIL_USERNAME"] = "anon.ak.herokuapp@gmail.com"
-app.config["MAIL_PASSWORD"] = "Fpmzq-onxw@0531"
+app.config["MAIL_USERNAME"] = "email_id"
+app.config["MAIL_PASSWORD"] = "password"
 # app.config[MAIL_PASSWORD] = os.environ['USER_PASSWORD']
 mail = Mail(app)
 
